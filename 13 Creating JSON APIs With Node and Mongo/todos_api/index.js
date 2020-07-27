fc5ca8712e3b18c1953845b9770789ec349520cd
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-27 16:00:32 +0300

    13 Creating JSON APIs With Node and Mongo is finished

--- 13 Creating JSON APIs With Node and Mongo/todos_api/index.js
+++ 13 Creating JSON APIs With Node and Mongo/todos_api/index.js
@@ -1,17 +1,19 @@
 var express = require('express'),
   app = express();
+var port = 5500;
+bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.send('HI THERE FROM EXPRESS!!!');
+  res.send('HELLO FROM THE ROOT ROUTE');
 });
 
-app.get('/happy', function (req, res) {
-  res.send(':)');
-});
+app.use('/api/todos', todoRoutes);
 
-var process = {};
-process.env = { PORT: 5500 };
-
-app.listen(process.env.PORT, function () {
-  console.log('Hello World!', process.env.PORT);
+app.listen(port, function () {
+  console.log('Hello World!', 5500);
 });
