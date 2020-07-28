7b236209a7a83e4a77c6451c73b9f9e8809dc45f
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-28 15:49:12 +0300

    14 Codealong Single Page Todo List with Express Mongo and jQuery is in progress

--- 14 Codealong Single Page Todo List with Express Mongo and jQuery/todos_api/index.js
+++ 14 Codealong Single Page Todo List with Express Mongo and jQuery/todos_api/index.js
@@ -1,19 +1,23 @@
 var express = require('express'),
-  app = express();
-var port = 5500;
-bodyParser = require('body-parser');
-
-var todoRoutes = require('./routes/todos');
+    app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+    
+var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/views'));
 
-app.get('/', function (req, res) {
-  res.send('HELLO FROM THE ROOT ROUTE');
+app.get('/', function(req, res){
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(port, function () {
-  console.log('Hello World!', 5500);
-});
+app.listen(port, function(){
+    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
+})
+    
+    
--- 14 Codealong Single Page Todo List with Express Mongo and jQuery/todos_api/models/index.js
+++ 14 Codealong Single Page Todo List with Express Mongo and jQuery/todos_api/models/index.js
@@ -4,4 +4,4 @@ mongoose.connect('mongodb://localhost/todo-api');
 
 mongoose.Promise = Promise;
 
-module.exports.Todo = require('./todo');
+module.exports.Todo = require("./todo");
