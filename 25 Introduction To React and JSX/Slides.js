77bd81cfc76a736f79df4ff1c5ac5607643ffcfd
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-03 13:02:47 +0300

    25 Introduction To React and JSX is finished!

--- 25 Introduction To React and JSX/Slides.js
+++ 25 Introduction To React and JSX/Slides.js
@@ -1,1 +1,101 @@
 /* Introduction To React and JSX
+
+React:
+A Front-End Framework
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Objectives
+
+Define front-end frameworks
+
+Describe React at a high level
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Front-End Frameworks
+
+JavaScript libraries that handle DOM manipulation
+
+Handles navigations (HTML5 push state)
+
+State management
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Front-End Framework
+
+React ------- GET / ------> Node.js
+
+React <---- index.html ---- Node.js
+
+React -- GET /bundle.js --> Node.js
+
+React <---- bundle.js  ---- Node.js
+(includes JS for framework)
+
+Framework is in control of the DOM
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+React
+
++ Release by Facebook in 2013
++ A view library that uses composable components
++ Other libraries are commonly used with React
+
+++ React Router
+++ Redux
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Based on Composable Components
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+========================================================
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+JSX
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Objectives
+
++ Define babel, a transpiler
++ Use JSX in our react component
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Transpiler: 
+converts from one source code version to another
+
+JSX -> Vanilla JS
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+JSX With JavaScript
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Objectives
+
++ Write JavaScript inside JSX
++ Use a style attribute in JSX
++ Add a className attribute
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Multiple Components
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Objectives
+
++ Render an array of JSX
++ Use a React component inside of another component
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
