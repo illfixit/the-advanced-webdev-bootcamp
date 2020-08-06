064e5b4545e6cb32d18d3051d7c266c07791393a
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-06 14:43:26 +0300

    35 Warbler Backend Part 2 is finished!

--- 35 Warbler Backend Part 2/warbler-server/index.js
+++ 35 Warbler Backend Part 2/warbler-server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 const db = require("./models");
 const PORT = 8081;
 
@@ -12,6 +14,26 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,
+  ensureCorrectUser,
+  messagesRoutes
+);
+
+app.get("/api/messages", loginRequired, async function(req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: "desc" })
+      .populate("user", {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.use(function(req, res, next) {
   let err = new Error("Not Found");
--- 35 Warbler Backend Part 2/warbler-server/models/user.js
+++ 35 Warbler Backend Part 2/warbler-server/models/user.js
@@ -18,7 +18,13 @@ const userSchema = new mongoose.Schema({
   },
   profileImageUrl: {
     type: String
-  }
+  },
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message"
+    }
+  ]
 });
 
 userSchema.pre("save", async function(next) {
